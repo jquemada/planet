b5f43f5fcf6e04e75ed99abf7fa695be7f4117d2
Juan Quemada <jquemada@dit.upm.es>
2011-12-13 22:13:12 +0100

    add seeds.rb for DB init

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,88 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Type.create(
+            name: 'Monumento',
+            description: 'Edificio de interes turistico o artistico')
+
+Type.create(
+            name: 'Naturaleza',
+            description: 'Lugar al aire libre de interes natural o de recreo')
+
+Type.create(
+            name: 'Ruina',
+            description: 'Lugar o resto de interes arquelogico')
+
+
+Site.create(
+            name: 'Pedriza',
+            description: 'Magnifico valle al norte de Madrid en el Macizo Central',
+            type_id: 2,
+            latitude: 40,
+            longitude: 40,
+            zoom: 10,
+            image_url: 'pedriza.png')
+
+Site.create(
+            name: 'Catedral de Florencia',
+            description: 'Catedral de la ciudad de Florencia con la que se inicia el Renacimiento',
+            type_id: 1,
+            latitude: 50,
+            longitude: 35,
+            zoom: 10,
+            image_url: 'florencia.png')
+
+Site.create(
+            name: 'Jardin de Lineo',
+            description: 'Jardin de la ciudad sueca de Uppsala donde el famoso naturalista enia su coleccion de plantas',
+            type_id: 2,
+            latitude: 80,
+            longitude: 45,
+            zoom: 10,
+            image_url: 'arbol1.png')
+
+Site.create(
+            name: 'Reichstag',
+            description: 'Parlamento aleman en la ciudad de Berlin',
+            type_id: 1,
+            latitude: 50,
+            longitude: 50,
+            zoom: 10,
+            image_url: 'reichstag.png')
+
+Site.create(
+            name: 'Pergamo',
+            description: 'Puerta del mercado de la antigua ciudad griega de Pergamo del museo arquelogico de Berlin',
+            type_id: 3,
+            latitude: 50,
+            longitude: 50,
+            zoom: 10,
+            image_url: 'pergamo.png')
+
+
+Trip.create(
+            name: 'Europa',
+            description: 'Viaje por ciudades europeas',
+            date: '2012-01-13')
+
+Trip.create(
+            name: 'Norte de Madrid',
+            description: 'Excursion a la sierra de Madrid',
+            date: '2012-04-13')
+
+Selected.create(
+                trip_id: 1,
+                site_id: 2,
+                hour: 11)
+
+Selected.create(
+                trip_id: 1,
+                site_id: 4,
+                hour: 15)
+
+Selected.create(
+                trip_id: 2,
+                site_id: 1,
+                hour: 10)
+
