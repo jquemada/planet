90ed7d91727981c784aa9edd4f0dfd300ff622d2
Victor H. Bazán <vh.bazan@upm.es>
2011-12-20 13:51:28 +0100

    include selected in view trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -14,6 +14,7 @@ class TripsController < ApplicationController
   # GET /trips/1.json
   def show
     @trip = Trip.find(params[:id])
+    @selected = Selected.new
 
     respond_to do |format|
       format.html # show.html.erb
