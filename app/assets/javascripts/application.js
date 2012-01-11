b50293f152d93faac4efbbaef9668d853305ec1b
Pablo Alonso Rodriguez y Victor Manuel Cano Fernandez <p.alonsor@alumnos.upm.es>
2012-01-11 10:38:05 +0100

    Realizadas limpieza de codigo y pruebas.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,6 @@ function displayPosition (){
   // Store my position (“y=mylat”, “x=mylong”) in variable “lating”
   var latlng = new google.maps.LatLng(mylat, mylong);
   
-  //var latlng2 = new google.maps.LatLng(0, 0);
   // Define my Google Maps options in variable “myOptions”
   var myOptions = {
     zoom: 1,
@@ -34,30 +33,16 @@ function displayPosition (){
     // Define map center
     mapTypeId: google.maps.MapTypeId.ROADMAP // tipo Roadmad, Satellite, Terrain, Hybrid
   };
-  
- 
-  
+
   // Load Google Map according to “myOptions” in Canvas identified by “map_canvas”
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  
-//   // Add marker in Google Maps “map” in position defined by “lating”
-//   var marker = new google.maps.Marker({
-//     position: latlng,
-//     map: map,
-//     title:"Wooolaaass"
-//   });
-//alert(createMarker(32, 32, siteImages[0]));
 
-  //colocaChinchetas();
- 
 }
+
 
 function colocaChinchetas(){
   
-  //alert('auch');
-  
-   for(i=0;i<latlongs.length;i++){
-       
+   for(i=0;i<latlongs.length;i++){    
       var marker = new google.maps.Marker({
 	position: latlongs[i],
 	map: map,
@@ -66,7 +51,6 @@ function colocaChinchetas(){
       });
       
     }
-    //window.open(imagenesCanvasArray[0].toDataURL());
 }
 
 function colocaNuevaChinchetaDelFinal(){
@@ -82,7 +66,6 @@ function errorPos(pos){alert('Error');}
 
 //Colocar posicion
 function colocaPos(pos){
-  //alert('Guay');
   $('.new_site #site_latitude').val(pos.coords.latitude);
   $('.new_site #site_longitude').val(pos.coords.longitude);
 }
@@ -101,59 +84,3 @@ $(function() {
 	alert('Error: Your navigator does not support geolocation');
   }
 });
-
-$('#new_selected').submit(function(){colocaChinchetas();});
-
-
-function createMarker(width, height, img) {
-
-  var canvas, context;
-  
-  radius=4;
-  
-  canvas = document.createElement("canvas");
-  canvas.width = width;
-  canvas.height = height;
-
-  context = canvas.getContext("2d");
-
-  //context.clearRect(0,0,width,height);
-
-  // border is black
-//  context.strokeStyle = "rgba(0,0,0,1)";
-//
-//  context.beginPath();
-//  context.moveTo(radius, 0);
-//  context.lineTo(width - radius, 0);
-//  context.quadraticCurveTo(width, 0, width, radius);
-//  context.lineTo(width, height - radius);
-//  context.quadraticCurveTo(width, height, width - radius, height);
-//  context.lineTo(radius, height);
-//  context.quadraticCurveTo(0, height, 0, height - radius);
-//  context.lineTo(0, radius);
-//  context.quadraticCurveTo(0, 0, radius, 0);
-//  context.closePath();
-//  
-//  var img = new Image();
-//  img.src=imageURL;
-// //Esperamos imagen
-      
-  
- 
-  
-  
-  
-  
-  context.drawImage(img, '1px', '1px', '31px', '31px');
-  
-  //context.fill();
-  
-  //context.stroke();
-
-  return canvas.toDataURL();
-
-}
-
-//$(document).ready(function(){
-//  alert(createMarker(32,32,'http://jonkepa.files.wordpress.com/2011/03/avioncito.jpg'));
-//});
