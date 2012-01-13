e81f9872a6d565a5ef208b37ae59c9b39b58790c
Pablo Alonso Rodriguez y Victor Manuel Cano Fernandez <p.alonsor@alumnos.upm.es>
2012-01-13 18:46:58 +0100

    Solucionando posibles incompatibilidades con PostgreSQL

--- app/helpers/mi_viajes_helper.rb
+++ app/helpers/mi_viajes_helper.rb
@@ -1,12 +1,12 @@
 module MiViajesHelper
   def dameSiguiente(mi_viaje)
-    candidato=MiViaje.where('order_in_trip LIKE ?',MiViaje.where('order_in_trip > ?',mi_viaje.order_in_trip).minimum('order_in_trip')).first
+    candidato=MiViaje.where('order_in_trip = ?',MiViaje.where('order_in_trip > ?',mi_viaje.order_in_trip).minimum('order_in_trip')).first
     candidato=mi_viaje if candidato.nil?
     return candidato
   end
   
   def dameAnterior(mi_viaje)
-    candidato=MiViaje.where('order_in_trip LIKE ?',MiViaje.where('order_in_trip < ?',mi_viaje.order_in_trip).maximum('order_in_trip')).first
+    candidato=MiViaje.where('order_in_trip = ?',MiViaje.where('order_in_trip < ?',mi_viaje.order_in_trip).maximum('order_in_trip')).first
     candidato=mi_viaje if candidato.nil?
     return candidato
   end
